c7fd5363d9bf1e97455d7f9f4eb1423b46d16e11
sofia-montoyam <123768481+sofia-montoyam@users.noreply.github.com>
2023-02-10 14:03:06 -0500

    Repository

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 document.addEventListener("DOMContentLoaded", () =>);
+   const cardAdj = [
+      {
+        name: "c",
+        img: "images/c.png"
+      },
+
+   ];
+
+   const cuadricula = document.querySelector(".cuadricula");
+   const resultado = document.querySelector("#resultado");
+   var cartasEscogidas = [];
+   var cartasEscogidasId = [];
+   var cartasGanadas = [];
+
+  });
