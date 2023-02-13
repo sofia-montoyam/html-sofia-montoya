d94e2acaf8550ea52a24a849a15ec8a1e4e40512
sofia-montoyam <123768481+sofia-montoyam@users.noreply.github.com>
2023-02-13 12:36:44 -0500

    Imágenes y texto

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () =>);
    const cardAdj = [
-      {
-        name: "c",
-        img: "images/c.png"
+      {name: "c", img: "images/c.png"}, {name: "cpp", img: "images/cpp.png"}, 
+      {name: "css", img: "images/css.png"}, {name: "fortran", img: "images/fortran.png"},
+      {name: "html", img: "images/html.png"}, {name: "python", img: "images/python.png"},
+      {name: "c", img: "images/c.png"}, {name: "csharp", img: "images/csharp.png"}, 
+      {name: "css", img: "images/css.png"}, {name: "fortran", img: "images/fortran.png"},
+      {name: "html", img: "images/html.png"}, {name: "python", img: "images/python.png"}];
+
+       
       },
 
    ];
